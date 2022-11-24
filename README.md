26755f6571d7766c016b1abbafdb588bcc489d6b
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2022-11-25 04:23:43 +0900

    Edit raspi_to_gpio

---
+++ README.md
@@ -0,0 +1,1 @@
+#crawler teleop by f310
--- crawler_teleop/raspi_to_gpio.py
+++ crawler_teleop/raspi_to_gpio.py
@@ -1,9 +1,11 @@
 import rclpy
 from rclpy.node import Node
+import RPi.GPIO as GPIO
 from std_msgs.msg import Int32MultiArray
 import serial
 import time
 
+bottom = 50
 R = 12
 L = 13
 ENABLE_r = 17
@@ -25,8 +27,9 @@ p_l.start(0)
 class SubscriberNode(Node):
     def __init__(self):
         super().__init__("subscriber")
+        self.joy_r = 0
+        self.joy_l = 0
 
-        time.sleep(0.1)
 
         self.verocity = None
         self.create_subscription(Int32MultiArray, "verocity", self.onSubscribed, 10)
@@ -38,10 +41,45 @@ class SubscriberNode(Node):
         print ("L: {0}, R: {1}".format(self.l, self.r))
 
     def toGpio(self, msg): 
-        self.r = 0
-        self.l = 0
-        r = msg.data[0]
-        l = msg.data[1]
+        self.joy_r = msg.data[0]
+        self.jop_l = msg.data[1]
+        motor_r = self.joy_r
+        motor_l = self.joy_l
+        time.sleep(0.1)
+            if motor_l > 0 and motor_r > 0:
+                GPIO.output(ENABLE_r, GPIO.LOW)
+                GPIO.output(ENABLE_l, GPIO.LOW)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
+                print("go:", motor_l, motor_r)
+            
+            elif motor_l > 0 and motor_r < 0:
+                GPIO.output(ENABLE_r, GPIO.HIGH)
+                GPIO.output(ENABLE_l, GPIO.LOW)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(-(motor_r))
+                print("turn right:", motor_l, motor_r)
+            
+            elif motor_l < 0 and motor_r > 0:
+                GPIO.output(ENABLE_r, GPIO.LOW)
+                GPIO.output(ENABLE_l, GPIO.HIGH)
+                p_r.ChangeDutyCycle(-(motor_l))
+                p_l.ChangeDutyCycle(motor_r)
+                print("turn left:", motor_l, motor_r)
+            
+            elif motor_l < 0 and motor_r < 0:
+                GPIO.output(ENABLE_r, GPIO.HIGH)
+                GPIO.output(ENABLE_l, GPIO.HIGH)
+                p_r.ChangeDutyCycle(-(motor_l))
+                p_l.ChangeDutyCycle(-(motor_r))
+                print("back:", motor_l, motor_r)
+            
+            else:
+                print("stop:", motor_l, motor_r)
+                p_r.stop()
+                p_l.stop()
+                p_r.start(0)
+                p_l.start(0)
 
 
 
@@ -50,6 +88,7 @@ def main(args=None):
     node = SubscriberNode()
     rclpy.spin(node)
     node.destroy_node()
+    GPIO.cleanup()
     rclpy.shutdown()
 
 if __name__ == "__main__":
